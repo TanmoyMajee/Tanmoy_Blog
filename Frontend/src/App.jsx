aee7e244d8e49b800cad5aeeb959f8460b6164c3
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-06-11 17:22:56 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 import Hero from './components/Hero'
-import Footer from './components/Footer'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+function Home(){
+  return(
+    <>
+      <Hero/>
+      <Projects/>
+    </>
+  )
+}
 
 function App() {
-  
-
   return (
-    <>
-      <Header/>
-      <Hero/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* wrap every route with layout  */}
+          <Route path='/' element={<Layout/>}>
+              <Route path='' element={<Home/>}/>
+              <Route path='/projects' element={<Projects/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='*' element={<NotFound/>}/>
+          </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

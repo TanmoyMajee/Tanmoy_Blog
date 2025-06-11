23d0ecef2aa26acbb3de48175aceb7914265972e
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-06-11 17:32:30 +0530

    chage ui

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,27 +4,27 @@ import { SiLeetcode } from 'react-icons/si'
 
 function Footer() {
   return (
-    <div className='flex flex-col justify-center items-center p-8 min-h-[200px] bg-gradient-to-b from-amber-50 to-amber-100'>
+    <div className='flex flex-col justify-center items-center p-8 min-h-[200px] bg-gradient-to-r from-gray-900 to-gray-800'>
       {/* div for logo and social media links */}
       <div className='flex gap-6 p-4'>
         <a href="https://github.com/TanmoyMajee" target="_blank"
           rel="noopener noreferrer"
-          className='text-3xl hover:scale-110 transition-all'>
+          className='text-3xl text-gray-300 hover:scale-110 transition-all'>
           <FaGithub />
         </a>
         <a href="https://www.linkedin.com/in/tanmoy-majee-2b7280288/" target="_blank"
           rel="noopener noreferrer"
-          className='text-3xl hover:scale-110 transition-all'>
+          className='text-3xl text-gray-300 hover:scale-110 transition-all'>
           <FaLinkedin />
         </a>
         <a href="https://leetcode.com/u/tanmoymajee239/" target="_blank"
           rel="noopener noreferrer"
-          className='text-3xl hover:scale-110 transition-all'>
+          className='text-3xl text-gray-300 hover:scale-110 transition-all'>
           <SiLeetcode />
         </a>
       </div>
       {/* div for content info */}
-      <div className='text-xl font-semibold mt-4'>TANMOY MAJEE ©2025</div>
+      <div className='text-xl text-gray-300 font-semibold mt-4'>TANMOY MAJEE ©2025</div>
     </div>
   )
 }
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,51 +1,48 @@
 
 import React from 'react'
-import { Link,  NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Header() {
   return (
-    <div className='flex justify-between items-center px-6 py-4 bg-gradient-to-r from-amber-50 to-amber-100 shadow-sm border-b border-amber-200'>
-      <Link 
-        to="/" 
+    <div className=' flex justify-between items-center px-6 py-4 bg-gradient-to-r from-gray-900 to-gray-800 shadow-sm border-b border-amber-200 '>
+      <Link
+        to="/"
         className='text-3xl font-bold text-amber-800 hover:text-amber-900 duration-200'
       >
         Tanmoy
       </Link>
-      
-      <nav className='flex gap-8'>
-        <NavLink 
-          to="/" 
-          className={({ isActive }) => 
-            `text-lg font-medium hover:text-amber-700 hover:underline underline-offset-4 decoration-2 ${
-              isActive 
-                ? 'text-amber-800 underline decoration-amber-600' 
-                : 'text-gray-700'
+
+      <nav className='flex gap-8 '>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `text-lg  font-medium hover:text-amber-700 hover:underline underline-offset-4 decoration-2 ${isActive
+              ? 'text-amber-800 underline decoration-amber-600'
+              : 'text-gray-300'
             }`
           }
         >
           Home
         </NavLink>
-        
-        <NavLink 
-          to="/projects" 
-          className={({ isActive }) => 
-            `text-lg font-medium hover:text-amber-700 hover:underline underline-offset-4 decoration-2 ${
-              isActive 
-                ? 'text-amber-800 underline decoration-amber-600' 
-                : 'text-gray-700'
+
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            `text-lg font-medium hover:text-amber-700 hover:underline underline-offset-4 decoration-2 ${isActive
+              ? 'text-amber-800 underline decoration-amber-600'
+              : 'text-gray-300'
             }`
           }
         >
           Projects
         </NavLink>
-        
-        <NavLink 
-          to="/contact" 
-          className={({ isActive }) => 
-            `text-lg font-medium hover:text-amber-700 hover:underline underline-offset-4 decoration-2 ${
-              isActive 
-                ? 'text-amber-800 underline decoration-amber-600' 
-                : 'text-gray-700'
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            `text-lg font-medium hover:text-amber-700 hover:underline underline-offset-4 decoration-2 ${isActive
+              ? 'text-amber-800 underline decoration-amber-600'
+              : 'text-gray-300'
             }`
           }
         >
